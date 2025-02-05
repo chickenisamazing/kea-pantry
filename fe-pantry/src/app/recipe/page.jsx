e064d2b7903952bbacaddf1dd49863d74a7727ca
chickenisamazing <dieswing@naver.com>
2025-02-05 12:03:50 +0900

    feat: Add get recipe data code in recipe list component (CSR)

--- fe-pantry/src/app/recipe/page.jsx
+++ fe-pantry/src/app/recipe/page.jsx
@@ -1,29 +1,53 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
 import styles from "./page.module.scss";
 
 export default function Page() {
   const router = useRouter();
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const buttons = [1, 2, 3, 4, 5];
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch(`http://localhost:3000/api/recipes`);
+        if (!res.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await res.json();
+        setDatas(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  // const buttons =  [1, 2, 3, 4, 5];
   const buttonOnClick = (num) => {
     router.push(`/recipe/${num}`);
   };
 
+  if (loading) return <div>Loading...</div>;
+
   return (
-    <div className={styles.title}>
-      <div>레시피 리스트 페이지</div>
-      {buttons.map((num) => (
-        <button
-          key={num}
-          onClick={() => buttonOnClick(num)}
-          className={styles.btn}
-        >
-          {num}
-        </button>
-      ))}
+    <div>
+      <div className={styles.title}>
+        <div className={styles.board}>레시피 리스트 페이지</div>
+        {datas.map((data) => (
+          <button
+            key={data.recipeId}
+            onClick={() => buttonOnClick(data.recipeId)}
+            className={styles.btn}
+          >
+            {data.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
