3309257f2fa73db38f6b4b814b5c0f8f1830f802
chickenisamazing <dieswing@naver.com>
2025-02-06 14:28:01 +0900

    feat: Add routing in some navbar category

--- fe-pantry/src/component/Navbar.jsx
+++ fe-pantry/src/component/Navbar.jsx
@@ -1,17 +1,22 @@
 "use client";
 
+import Link from "next/link";
 import styles from "./Navbar.module.scss";
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <a className={styles[`navbar-title`]}>
+      <Link href="/" className={styles[`navbar-title`]}>
         <img src="/parakeet.png" alt="File icon" width={64} height={64} />
         KEA's pantry
-      </a>
+      </Link>
       <div className={styles[`navbar-items`]}>
-        <a className={styles[`navbar-item`]}>레시피</a>
-        <a className={styles[`navbar-item`]}>나의 팬트리</a>
+        <Link href="/recipe" className={styles[`navbar-item`]}>
+          레시피
+        </Link>
+        <Link href="/pantry" className={styles[`navbar-item`]}>
+          나의 팬트리
+        </Link>
         <a className={styles[`navbar-item`]}>레시피 추천</a>
       </div>
       <a className={styles[`navbar-admin`]}>admin</a>
