44d7fd86001f61f79a7a112bede4786fbe86c715
chickenisamazing <dieswing@naver.com>
2025-02-07 16:02:31 +0900

    feat: Add get temp pantry data in pantry page

--- fe-pantry/src/app/pantry/page.jsx
+++ fe-pantry/src/app/pantry/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import styles from "./page.module.scss";
 
 export default function Page() {
   const [ingredients, setIngredients] = useState([]);
@@ -26,17 +27,31 @@ export default function Page() {
   }, []);
 
   return (
-    <div>
-      <div>팬트리 페이지</div>{" "}
-      {ingredients.map((ingredient) => (
-        <button
-          key={ingredient.id}
-          onClick={() => buttonOnClick(ingredient.id)}
-          //   className={styles.btn}
-        >
-          {ingredient.name}
-        </button>
-      ))}
+    <div className={styles.house}>
+      <div className={styles.title}>나의 팬트리</div>{" "}
+      <div className={styles.container}>
+        {ingredients.map((ingredient) => (
+          <div className={styles.ingredient} key={ingredient.id}>
+            <div
+              // key={ingredient.id}
+              onClick={() => buttonOnClick(ingredient.id)}
+            >
+              {ingredient.name}
+            </div>
+            <img
+              // key={ingredient.id}
+              src={ingredient.image}
+              alt="File icon"
+              width={80}
+              height={80}
+            />
+            <div className={styles.amount}>
+              <div>{ingredient.quantity}</div>
+              <div>{ingredient.unit}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
