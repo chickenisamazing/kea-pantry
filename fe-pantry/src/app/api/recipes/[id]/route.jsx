73bd03a807172d93233c3f62570a27408ca086a7
chickenisamazing <dieswing@naver.com>
2025-02-14 13:05:14 +0900

    fix: Modify dummy data name in recipe detail api routes

--- fe-pantry/src/app/api/recipes/[id]/route.jsx
+++ fe-pantry/src/app/api/recipes/[id]/route.jsx
@@ -2,8 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, context) {
   const { params } = await context;
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/data/dummyRecipeData.json`);
+
+  const API_URL =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://kea-pantry.vercel.app";
+
+  const res = await fetch(`${API_URL}/data/dummyVercelTestRecipeData.json`);
 
   if (!res.ok) {
     return NextResponse.error();
@@ -11,7 +16,10 @@ export async function GET(request, context) {
 
   const data = await res.json();
   const recipe = data.recipes;
-  const oneRecipe = recipe.find((r) => r.recipeId === Number(params.id));
+  console.log(recipe, "레시피");
+  const oneRecipe = recipe.find(
+    async (r) => r.recipeId === Number(await params.id)
+  );
 
   if (!oneRecipe) {
     return NextResponse.json({ error: "Recipe not found" }, { status: 404 });
