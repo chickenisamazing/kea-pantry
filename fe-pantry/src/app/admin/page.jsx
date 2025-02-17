aa1676f301a908ca19ec2dd16fe5d0e5cdbcff27
chickenisamazing <dieswing@naver.com>
2025-02-17 21:54:41 +0900

    feat: Add input and button in admin page

--- fe-pantry/src/app/admin/page.jsx
+++ fe-pantry/src/app/admin/page.jsx
@@ -2,8 +2,18 @@ import styles from "./page.module.scss";
 
 export default function Page() {
   return (
-    <div>
-      <div>어드민 페이지입니다.</div>
+    <div className={styles.containter}>
+      <div className={styles.require}>관리자 로그인</div>
+
+      <div className={styles.content}>
+        <div className={styles.inputs}>
+          <input className={styles["input-style"]} placeholder="아이디" />
+          <input className={styles["input-style-2"]} placeholder="비밀번호" />
+        </div>
+        <div>
+          <div className={styles.btn}>로그인</div>
+        </div>
+      </div>
     </div>
   );
 }
